fa6f6b049e0b548a1f944d943d4fd9ddb28942c8
WaltOlivia <livwalton1@gmail.com>
2026-02-05 09:05:43 -0700

    hello

--- src/app.jsx
+++ src/app.jsx
@@ -1,33 +1,55 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Scores } from './scores/scores';
+import { About } from './about/about';
 
 export default function App() {
-  return   (
+  return   ( <BrowserRouter>
+
   <div className="app bg-dark text-light">
     <header className="container-fluid">
-      <nav className="navbar fixed-top navbar-dark">
-        <a className="navbar-brand" href="#">Simon<sup>&reg;</sup></a>
-        <menu className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link active" href="index.html">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="play.html">Play</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="scores.html">Scores</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="about.html">About</a>
-          </li>
-        </menu>
-      </nav>
+        <nav className="navbar fixed-top navbar-dark">
+            <div className="navbar-brand">
+                Simon<sup>&reg;</sup>
+            </div>
+            <menu className="navbar-nav">
+                <li className="nav-item">
+                <NavLink className="nav-link" to="">
+                    Login
+                </NavLink>
+                </li>
+                <li className="nav-item">
+                <NavLink className="nav-link" to="play">
+                    Play
+                </NavLink>
+                </li>
+                <li className="nav-item">
+                <NavLink className="nav-link" to="scores">
+                    Scores
+                </NavLink>
+                </li>
+                <li className="nav-item">
+                <NavLink className="nav-link" to="about">
+                    About   
+                </NavLink>
+                </li>
+            </menu>
+        </nav>
     </header>
 
-    <main className="container-fluid bg-secondary text-center">
-        componants go here
-    </main>
+
+
+    <Routes>
+        <Route path='/' element={<Login />} exact />
+        <Route path='/play' element={<Play />} />
+        <Route path='/scores' element={<Scores />} />
+        <Route path='/about' element={<About />} />
+        <Route path='*' element={<NotFound />} />
+    </Routes>
 
     <footer className="bg-dark text-white-50">
       <div className="container-fluid">
@@ -35,5 +57,10 @@ export default function App() {
         <a className="text-reset" href="https://github.com/webprogramming260/simon-css">Source</a>
       </div>
     </footer>
-  </div> );
+  </div> 
+  </BrowserRouter>);
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
---
+++ src/play/play.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Login() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>play displayed here</div>
+    </main>
+  );
+}
